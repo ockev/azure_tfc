8058533a0d2c9b936e89e762db9f9b3ff6ac859d
ockev <koconnor@hashicorp.com>
2023-03-28 11:36:26 -0400

    first commit

---
+++ project-qa/backend.tf
@@ -0,0 +1,32 @@
+
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+
+    backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "koconnor"
+
+    workspaces {
+      name = "Azure_TFC_Prod"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
